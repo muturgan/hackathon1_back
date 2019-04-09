69a86695677ef262e18a79176954873a73cd7438
Andrey Sakharov <sakharov.a@ittest-team.ru>
2019-04-09 06:29:07 +0300

    fill the images db table

---
+++ sql/images.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `images`
+(
+    `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+	`name` TEXT NOT NULL,
+	`path` TEXT NOT NULL,
+	`preview` TEXT NOT NULL,
+	`tags` JSON,
+	`likedUsers` JSON,
+	`likes` INT UNSIGNED DEFAULT 0
+) ENGINE = InnoDB;
---
+++ src/util/collectImages.ts
@@ -0,0 +1,68 @@
+import { request, RequestOptions } from 'https';
+import {knex} from '../services/db-driver';
+
+
+
+const getData = (): Promise<any> => {
+
+    return new Promise((resolve, reject) => {
+        const options: RequestOptions = {
+            hostname: 'cloud-api.yandex.net',
+            path: '/v1/disk/resources?path=hackassets&limit=100',
+            method: 'GET',
+            headers: {
+                Authorization: `OAuth AQAEA7qiS2gyAAWYSZBhcSnGs09ZvmKid5aEmPk`,
+                ['Content-Type']: 'application/json',
+            },
+        };
+
+        const req = request(options, res => {
+            if (res.statusCode !== 200) {
+                return reject(res.statusMessage);
+            }
+
+            const arr: Array<Buffer> = [];
+
+            res.on('data', (chunk: Buffer) => {
+                arr.push(chunk);
+            });
+
+            res.on('end', () => {
+                try {
+                    const data = JSON.parse( (arr as any as Buffer).toString('utf8') );
+                    resolve(data);
+
+                } catch (err) { throw err; }
+            });
+        });
+
+        req.on('error', (error) => {
+            return reject(error);
+        });
+
+        req.end();
+    });
+};
+
+
+const collectImage = async () => {
+    try {
+        const data = await getData();
+        const items = data._embedded.items as Array<{[key: string]: string}>;
+
+        const insertedIds: number[] = await Promise.all(
+            items.map(item => knex('images')
+                .insert({
+                    name: item.name,
+                    path: item.file,
+                    preview: item.preview,
+                    tags: JSON.stringify([]),
+                    likedUsers: JSON.stringify([]),
+                })
+            )
+        );
+
+        return {insertedIds};
+
+    } catch (err) { throw err; }
+};
